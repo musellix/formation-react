696757cf8e908d5d8cb3eb0c4ff00c97b5b221bc
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:11:34 +0100

    3.9 - Les bases de React - Rendu conditionnel if

--- 3.Les-bases-de-React/src/lessons/9.Rendu-conditionnel-if/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/9.Rendu-conditionnel-if/Container.jsx
@@ -1,5 +1,15 @@
 export default function Container() {
+
+  const isLogged = true;
+
+  if(isLogged) {
+    content = "Vous etes connecté"
+  }
+  else {
+    content = "Vous n'etes pas connecté"
+  }
+
   return <div>
-    <h1>Rendu conditionnel</h1>
-  </div>
+      <h1>{content}</h1>
+    </div>
 }
