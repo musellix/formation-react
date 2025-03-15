58e87c0871071c1e30388b47de05b5e0ed18a6fc
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:52:28 +0100

    6.3 - Creer des composants courants - Coder une navbar

--- 6.Créer-des-composants-courants/src/App.jsx
+++ 6.Créer-des-composants-courants/src/App.jsx
@@ -1,10 +1,10 @@
-import Tabs from "./lessons/2.Coder-des-onglets/Tabs"
+import Navbar from "./lessons/3.Coder-une-navbar/Navbar"
 
 function App() {
 
   return (
     <div className="min-h-screen bg-slate-700 pt-10">
-      <Tabs/>
+      <Navbar/>
     </div>
   )
 }
