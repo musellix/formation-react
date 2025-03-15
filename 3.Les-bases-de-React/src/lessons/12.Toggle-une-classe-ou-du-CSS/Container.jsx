7f28d943a7160c87886b7f11a60ee6eb6b2258b5
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:12:40 +0100

    3.12 - Les bases de React - Toggle une classe ou du CSS

--- 3.Les-bases-de-React/src/lessons/12.Toggle-une-classe-ou-du-CSS/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/12.Toggle-une-classe-ou-du-CSS/Container.jsx
@@ -1,9 +1,19 @@
 import "./Container.css"
+import { useState } from "react"
 
 export default function Container() {
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div>
+    <div className={`full-container ${darkMode && "dark-mode"}`}>
       <h1>Toggle une classe</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita inventore in blanditiis vel iste! Iure, reprehenderit tempora. Beatae laboriosam, ad eum nesciunt ipsam porro error, officiis ea id, voluptate ratione.</p>
+    
+      <button
+        onClick={() => setDarkMode(!darkMode)}
+      >
+      Activer le {darkMode ? 'light mode' : 'dark mode'}
+      </button>
     </div>
   )
 }
