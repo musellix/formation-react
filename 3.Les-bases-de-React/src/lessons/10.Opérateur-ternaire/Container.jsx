69b024e7309aee31eb22f5845dc97da3cb109618
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:11:55 +0100

    3.10 - Les bases de React - Operateur ternaire

--- 3.Les-bases-de-React/src/lessons/10.Opérateur-ternaire/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/10.Opérateur-ternaire/Container.jsx
@@ -2,10 +2,19 @@ import { useState } from "react"
 import "./Container.css"
 
 export default function Container() {
+
+  const [validation, setValidation] = useState(false);
+
   return (
     <div>
       <h1>Opération ternaire</h1>
-      <form></form>
+      <form>
+        <input onClick={() => setValidation(!validation)} type="checkbox"></input>
+        <p
+          style={{color: `${validation ? "loghtgreen" : "red"}`}}>
+            {validation ? "Bravo" : "Cocher la case"}
+          </p>
+      </form>
     </div>
   )
 }
