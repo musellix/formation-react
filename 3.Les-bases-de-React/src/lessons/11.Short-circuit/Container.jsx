c00305ad6aeed3306bf8318122d9de6788ae649d
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:12:15 +0100

    3.11 - Les bases de React - Short circuit

--- 3.Les-bases-de-React/src/lessons/11.Short-circuit/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/11.Short-circuit/Container.jsx
@@ -1,10 +1,10 @@
 export default function Container() {
+const isLogged = false;
 
   return (
     <div>
       <h1>Bienvenue sur BooksParadise</h1>
-
-
+      {isLogged && <button>Afficher</button>}
     </div>
   )
 }
