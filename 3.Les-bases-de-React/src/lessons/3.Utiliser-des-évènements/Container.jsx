982c8c524173c8c28a125305f679491a8574a4c8
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:08:53 +0100

    3.3 - Les bases de React - Utiliser des evenements

--- 3.Les-bases-de-React/src/lessons/3.Utiliser-des-évènements/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/3.Utiliser-des-évènements/Container.jsx
@@ -4,11 +4,14 @@ export default function Container() {
     <div>
       <h1>Utiliser des évènements</h1>
       {/* On peut ajouter un évènement directement dans le JSX avec une fonction anonyme, mais ce n'est pas pratique s'il y a beaucoup de code */}
+      <button onClick={() => console.log("click")}>Click</button>
+
 
       {/* Mauvaise pratique, peu maintenable */}
 
 
       {/* On lui préfère en général ceci */}
+      <button onClick={handleClick}>Click</button>
 
 
       {/* Attention, si on veut passer un argument, il faut obligatoirement utiliser une fonction anonyme */}
