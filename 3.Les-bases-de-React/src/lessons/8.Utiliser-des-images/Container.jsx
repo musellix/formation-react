704d79473c62e99b1e4c8dba43f7628bb8d2e80d
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:11:16 +0100

    3.8 - Les bases de React - Utiliser des images

--- 3.Les-bases-de-React/src/lessons/8.Utiliser-des-images/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/8.Utiliser-des-images/Container.jsx
@@ -1,9 +1,19 @@
+import fuji from "./assets/fuji.jpg"
+
 export default function Container() {
+
+  console.log(fuji)   // retourne l'url de l'image
 
   return (
     <div>
       <h1>Utiliser des images</h1>
+      <img src={fuji} alt="Mont Fuji"></img>
 
+      <p>Utiliser des images du dossier public</p>
+      <img src="/assets/forest-1.jpg"></img>        {/* image dans le dossier public */}
+      <img src={`/assets/forest-${id}.jpg`}></img>    {/* image dans le dossier public */}
+
+      <p>SVG</p>
     </div>
   )
 }
