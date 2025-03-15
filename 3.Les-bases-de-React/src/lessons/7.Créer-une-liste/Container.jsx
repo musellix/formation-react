a97202283064f0428750ab2744cf5e7aacf596a0
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:11:00 +0100

    3.7 - Les bases de React - Creer une liste

--- 3.Les-bases-de-React/src/lessons/7.Créer-une-liste/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/7.Créer-une-liste/Container.jsx
@@ -1,7 +1,26 @@
 export default function Container() {
+
+  const data = [
+    {
+      id: 1,
+      name: "Alice"
+    }, {
+      id: 2,
+      name: "Elliott"
+    }, {
+      id: 3,
+      name: "Bene"
+    }
+  ]
+
   return (
     <div>
       <h1>Créer une liste d'éléments avec React</h1>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
