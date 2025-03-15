489fcb6038b5f844d3fe42f2dce3aacd741c821d
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:09:15 +0100

    3.4 - Les bases de React - Le state

--- 3.Les-bases-de-React/src/lessons/4.Le-state/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/4.Le-state/Container.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 export default function Container() {
+
+  // useState retourne un table avec la valeur, 
+  // et la methode pour modifier la valeur
+  // le count demarre à 0
+  const [count, setCount] = useState(0)
+
+  // modifier la valeur du count
+  setCount(count + 1)
 
   return (
     <div>
       <h1>Le state</h1>
+
     </div>
   )
 }
