bd29f3fbad9bcd75fcf9707e089c5c841ab2a3ef
xavier.muselli <xavier.muselli@sii.fr>
2025-03-15 22:23:14 +0100

    5.3 - React dans le detail - Le hook useEffect

--- 5.React-dans-le-détail/src/App.jsx
+++ 5.React-dans-le-détail/src/App.jsx
@@ -1,4 +1,4 @@
-import Container from "./lessons/2.La-règle-des-hooks/Container"
+import Container from "./lessons/3.Le-hook-use-effect/Container"
 
 function App() {
 
--- 5.React-dans-le-détail/src/lessons/3.Le-hook-use-effect/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/3.Le-hook-use-effect/Container.jsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from "react"
 import "./Container.css"
 
 export default function Container() {
-
+  const [count, setCount] = useState(0)
   
+  useEffect( () => {
+    console.log( "apres premiere creation et apres mise à jour du DOM")
+  }, [count])
+
+  console.log(count)
+
   return (
     <div>
       <h1>Le hook useEffect</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   )
 }
