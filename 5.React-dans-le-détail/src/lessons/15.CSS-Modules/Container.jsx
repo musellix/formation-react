40f0a85b8344637689ee580d0e68c272f69b4ac4
Musellix <musellix@gmail.com>
2025-03-15 22:42:39 +0100

    5.15 - React dans le detail - CSS Modules

--- 5.React-dans-le-détail/src/lessons/15.CSS-Modules/Container.jsx
+++ 5.React-dans-le-détail/src/lessons/15.CSS-Modules/Container.jsx
@@ -1,8 +1,12 @@
+import './Container.css'
+import style from "./Container.module.css"
+
 export default function Container() {
 
   return (
     <div>
-      <h1>CSS Modules</h1>
+      <h1 className="title">CSS Modules</h1>
+      <h1 className={style.title}>CSS Modules</h1>
     </div>
   )
 }
