6bfbe60070854fc9cb5dc64896cd37f56d8d7683
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-21 17:02:50 +0100

    9 - React Redux - projet Fruity - Afficher les fruits selectionnés

--- 9.React-Redux/src/pages/Fruity/FruityCart.jsx
+++ 9.React-Redux/src/pages/Fruity/FruityCart.jsx
@@ -10,11 +10,28 @@ export default function FruityCart() {
         <p className='text-xl p-5 border-b border-slate-400'>Your FruityCart</p>
 
         <ul>
-
-
+          {fruityCart.cart.length > 0 && fruityCart.cart.map(fruit => (
+            <li
+              className='px-5 py-2 text-xl'
+              key={fruit.id}
+            >
+              <span className='font-semibold mr-2'>
+                {fruit.quantity}
+              </span>
+              <span>{fruit.name}</span>
+            </li>
+          ))}
+          {fruityCart.cart.length === 0 && (
+            <li className='px-5 py-2 text-xl font-semibold'>Grab one fruit</li>
+          )}
         </ul>
         <p className='text-xl p-5 border-t border-slate-400'>
             Total price :
+            {fruityCart.cart.length > 0 && (
+              <span>{fruityCart.cart.reduce((acc, item) => {
+                return acc + item.quantity * item.price  
+              }, 0)}</span>
+            )}
         </p>
     </div>
 
