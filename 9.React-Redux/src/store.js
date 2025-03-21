a567a7e58863070152a66e94e6fadd534d39258c
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-21 17:51:27 +0100

    9 - React Redux - les custom middlewares

--- 9.React-Redux/src/store.js
+++ 9.React-Redux/src/store.js
@@ -8,5 +8,26 @@ export const store = configureStore({
         counter,
         fruits,
         fruitsCart,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(customMiddleware)
 })
+
+function customMiddleware(store) {
+    return function(next) {
+        return function(action) {
+            // console.log(store.getState())
+            // console.log(next)
+
+            // a chaque fois qu'on fait une action, n'importe laquelle, on intercepte et on fait addOne :)
+            next({
+                type: 'fruitsCart/addOne',
+                payload: {
+                    name: "Mango",
+                    url: "/images/mango.jpg",
+                    price: 999,
+                    id: 65237894
+                }
+            })
+        }
+    }
+}
