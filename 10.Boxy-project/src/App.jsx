08f36a260778634b6a9377dd8f554979d7aa3110
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-27 11:39:11 +0100

    10 - Projet Boxy-generator - header et footer

--- 10.Boxy-project/src/App.jsx
+++ 10.Boxy-project/src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex flex-wrap justify-center items-center flex-grow bg-gray-100 px-10 md:flex-nowrap">
+        {/* <LeftSide /> */}
+        {/* <Visualization /> */}
+      </main>
+        <Footer />
     </div>
   );
 }
