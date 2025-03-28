e41bbf3a8a6b13b0c767b51da74db288986d6114
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-28 11:33:43 +0100

    10 - Projet Boxy-generator - composants des proprietes de la box

--- 10.Boxy-project/src/layouts/BoxPanel/BoxPanel.jsx
+++ 10.Boxy-project/src/layouts/BoxPanel/BoxPanel.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import BoxRange from './BoxRange'
+import BoxColorPicker from './BoxCOlorPicker'
 
 export default function BoxPanel() {
+  const boxState = useSelector(state => state.boxProperties)
+
+  const boxInputs = boxState.map((input, index) => {
+    if( input.type === 'range' ) {
+      return <BoxRange key={index} inputData={input} />
+    }
+    else if( input.type === "color") {
+      return <BoxColorPicker key={index} inputData={input} />
+    }
+  })
+
   return (
-    <div>BoxPanel</div>
+    <div className='bg-gray-50 px-6 py-4 border-b border-gray-300'>
+      <p className='font-bold text-lg my-2'>Box properties</p>
+      {boxInputs}
+    </div>
   )
 }
