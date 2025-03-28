4611c4bc5c09eb604d584576d3800ef68209bd73
Xavier Muselli <xavier.muselli@sii.fr>
2025-03-28 12:48:49 +0100

    10 - Projet Boxy-generator - input color

--- 10.Boxy-project/src/layouts/BoxPanel/BoxColorPicker.jsx
+++ 10.Boxy-project/src/layouts/BoxPanel/BoxColorPicker.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { updateBoxValue } from '../../features/boxProperties'
 
-export default function BoxColorPicker() {
+export default function BoxColorPicker({inputData}) {
+      const dispatch = useDispatch()
+  
+      function handleInputs(e) {
+          dispatch(updateBoxValue({
+              inputNumber: inputData.inputNumber,
+              value: e.target.value,
+          }))
+      }
+
   return (
-    <div>BoxColorPicker</div>
+    <div className='mt-3'>
+      <p>{inputData.name}</p>
+      <div className='flex mt-2'>
+
+        {/* le input text */}
+        <input 
+          type="text"
+          value={inputData.value}
+          onChange={handleInputs}
+          className='flex-grow border py-1 px-2 focus:outline-1 outline-gray-400'
+        />
+
+        {/* le input color */}
+        <input
+          type="color"
+          value={inputData.value}
+          onChange={handleInputs}
+          className='cursor-pointer h-[40px]'
+        />
+      </div>
+    </div>
   )
 }
